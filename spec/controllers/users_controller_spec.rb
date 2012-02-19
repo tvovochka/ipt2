d4a31d92fe6b558914f0bf642f3b4afdfc2a0bfc
tvovochka <t_vovochka@mail.ru>
2012-02-19 13:16:45 +0600

    Add admin in user model

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -47,11 +47,11 @@ describe UsersController do
       it "should paginate users" do
         get :index
         response.should have_selector("div.pagination")
-        response.should have_selector("span.disabled", :content => "Previous")
+        response.should have_selector("span.disabled", :content => "Предыдущие")
         response.should have_selector("a", :href => "/users?page=2",
                                            :content => "2")
         response.should have_selector("a", :href => "/users?page=2",
-                                           :content => "Next")
+                                           :content => "Следующие")
       end
     end
   end
